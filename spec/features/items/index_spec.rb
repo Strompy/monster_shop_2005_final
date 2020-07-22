e6392784186a25839bf4065f9d9e3e5dad7257aa
Chandler Hulstrom <chulstro11@gmail.com>
2020-07-22 16:13:46 -0600

    User story 18 is functional and tested

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe "Items Index Page" do
 
       @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
       @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+      @order_1 = Order.create(name: "John", address: "124 Lickit dr", city: "Denver", state: "Colorado", zip: 80890)
+
+      ItemOrder.create(item: @pull_toy, order: @order_1, quantity: 7, price: 43)
+      ItemOrder.create(item: @tire, order: @order_1, quantity: 3, price: 89)
     end
 
     # xit "all items or merchant names are links" do
@@ -83,10 +88,42 @@ RSpec.describe "Items Index Page" do
 
     end
 
-    # it "has popularity statistics" do
-    #   visit "/items"
-    #
-    #
-    # end
+
+  end
+
+  describe "When looking at the item index" do
+    before :each do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+      @pull_toy = @bike_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      @dog_bone = @bike_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+      @plane = @bike_shop.items.create(name: "Plane", description: "Yerp", price: 17, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 89)
+      @baklava = @bike_shop.items.create(name: "Baklava", description: "Flaky!", price: 78, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 43)
+
+      @order_1 = Order.create(name: "John", address: "124 Lickit dr", city: "Denver", state: "Colorado", zip: 80890)
+
+      ItemOrder.create(item: @pull_toy, order: @order_1, quantity: 7, price: 89)
+      ItemOrder.create(item: @tire, order: @order_1, quantity: 3, price: 89)
+      ItemOrder.create(item: @plane, order: @order_1, quantity: 6, price: 89)
+      ItemOrder.create(item: @baklava, order: @order_1, quantity: 89, price: 89)
+      ItemOrder.create(item: @dog_bone, order: @order_1, quantity: 65, price: 89)
+      ItemOrder.create(item: @chain, order: @order_1, quantity: 1, price: 89)
+    end
+
+    it "has popularity statistics" do
+      visit "/items"
+
+      order = "#{@baklava.name} 89 #{@dog_bone.name} 65 #{@pull_toy.name} 7 #{@plane.name} 6 #{@tire.name} 3"
+      within "#pop" do
+        expect(page).to have_content(order)
+      end
+
+      reverse_order = "#{@chain.name} 1 #{@tire.name} 3 #{@plane.name} 6 #{@pull_toy.name} 7 #{@dog_bone.name} 65"
+      within "#least_pop" do
+        expect(page).to have_content(reverse_order)
+      end
+    end
   end
 end
