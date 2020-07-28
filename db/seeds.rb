153bcb4b06d86a94d75eedb40b268a496b4fb249
Chandler Hulstrom <chulstro11@gmail.com>
2020-07-28 15:26:44 -0600

    Seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,18 @@
 
 Merchant.destroy_all
 Item.destroy_all
+User.destroy_all
 
+#users
+john = User.create(name: "John", address: "145 Uvula dr", city: "Lake", state: "Michigan", zip: 80203, email: "tot@example.com", password: "password")
+jenn = User.create(name: "Jenn", address: "145 Uvula dr", city: "Lake", state: "Michigan", zip: 80203, email: "lols@example.com", password: "password")
 #merchants
 bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+toy_shop = Merchant.create(name: "Wizard's Toy Shop", address: '125 Spinny St.', city: 'Denver', state: 'CO', zip: 80203)
+tree_shop = Merchant.create(name: "Wizard's Toy Shop", address: '125 Spinny St.', city: 'Denver', state: 'CO', zip: 80203)
+magic_shop = Merchant.create(name: "Wizard's Toy Shop", address: '125 Spinny St.', city: 'Denver', state: 'CO', zip: 80203)
+snack_shop = Merchant.create(name: "Wizard's Toy Shop", address: '125 Spinny St.', city: 'Denver', state: 'CO', zip: 80203)
 
 #bike_shop items
 tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
@@ -20,10 +28,22 @@ chain = bike_shop.items.create(name: "Chain", description: "It'll never break!",
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
-plane = bike_shop.items.create(name: "Plane", description: "Yerp", price: 17, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 89)
-baklava = bike_shop.items.create(name: "Baklava", description: "Flaky!", price: 78, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 43)
 
-order_1 = Order.create(name: "John", address: "124 Lickit dr", city: "Denver", state: "Colorado", zip: 80890)
+#toy_shop items
+plane = toy_shop.items.create(name: "Plane", description: "Yerp", price: 17, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 89)
+baklava = toy_shop.items.create(name: "Baklava", description: "Flaky!", price: 78, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 43)
+
+#tree_shop items
+maple = tree_shop.items.create(name: "Maple Tree", description: "Tall and Strong", price: 56, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 22)
+
+#magic_shop items
+wand = magic_shop.items.create(name: "Wand", description: "Floating time", price: 45, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 33)
+
+#snack_shop items
+meat = snack_shop.items.create(name: "Beef Jerky", description: "Nummy", price: 12, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 78)
+
+order_1 = john.orders.create(name: "John", address: "124 Lickit dr", city: "Denver", state: "Colorado", zip: 80890)
+order_2 = jenn.orders.create(name: "Jenn", address: "234 Loser ln", city: "Denver", state: "Colorado", zip: 78975)
 
 ItemOrder.create(item: pull_toy, order: order_1, quantity: 7, price: 54)
 ItemOrder.create(item: tire, order: order_1, quantity: 3, price: 83)
@@ -31,3 +51,6 @@ ItemOrder.create(item: plane, order: order_1, quantity: 6, price: 83)
 ItemOrder.create(item: baklava, order: order_1, quantity: 89, price: 83)
 ItemOrder.create(item: dog_bone, order: order_1, quantity: 65, price: 83)
 ItemOrder.create(item: chain, order: order_1, quantity: 1, price: 83)
+ItemOrder.create(item: maple, order: order_2, quantity: 1, price: 83)
+ItemOrder.create(item: wand, order: order_2, quantity: 1, price: 83)
+ItemOrder.create(item: meat, order: order_2, quantity: 1, price: 83)
