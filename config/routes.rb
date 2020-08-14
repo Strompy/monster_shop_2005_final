355b57a16cc285e2e71ccad7fe4872239eb109c9
Phillip Strom <phillip.strom@gmail.com>
2020-08-14 10:58:12 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 
-  resources :merchants
+  resources :merchants do
+    resources :items, only: [:index, :new, :create]
+  end
 
-  resources :items, only: [:show, :index, :edit, :update, :destroy]
-  get "/merchants/:merchant_id/items", to: "items#index"
-  get "/merchants/:merchant_id/items/new", to: "items#new"
-  post "/merchants/:merchant_id/items", to: "items#create"
-
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id/reviews", to: "reviews#create"
+  resources :items, only: [:show, :index, :edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
 
   resources :reviews, only: [:edit, :update, :destroy]
 
@@ -20,15 +18,17 @@ Rails.application.routes.draw do
   delete "/cart", to: "cart#empty"
   delete "/cart/:item_id", to: "cart#remove_item"
 
-  resources :orders, only: [:new, :create, :show, :destroy]
-  patch "/orders/:order_id", to: 'orders#update'
+  resources :orders, only: [:new, :create, :show, :destroy, :update]
 
-  get "/register", to: 'users#new'
-  post "/register", to: 'users#create'
+  scope :register do
+    get "/", to: 'users#new'
+    post "/", to: 'users#create'
+  end
 
   get "/login", to: 'sessions#new'
   post "/login", to: 'sessions#create'
   delete "/logout", to: 'sessions#destroy'
+
 
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
@@ -39,33 +39,24 @@ Rails.application.routes.draw do
   get '/profile/orders/:order_id', to: 'user_orders#show'
 
   namespace :admin do
+    resources :merchants, only: [:show, :index, :destroy, :update] do
+      resources :items, only: [:index, :new, :create, :update, :edit]
+    end
+    resources :users, only: [:index, :show]
+    resources :orders, only: [:show]
+    resources :items, only: [:destroy] do
+      patch "/toggle", to: 'toggle_items#update'
+    end
     resources :dashboard, only: [:index]
-    get "/merchants/:merchant_id", to: "merchants#show"
     patch '/dashboard', to: 'dashboard#update'
-    get "/merchants", to: 'merchants#index'
-    delete "/merchants/:merchant_id", to: 'merchants#destroy'
-    patch "/merchants/:merchant_id", to: 'merchants#update'
-    get "/merchants/:merchant_id/items", to: 'items#index'
-    patch "/items/:item_id/toggle", to: 'toggle_items#update'
-    delete "/items/:item_id", to: 'items#destroy'
-    get "/merchants/:merchant_id/items/new", to: 'items#new'
-    post "/merchants/:merchant_id/items", to: 'items#create'
-    get "/merchants/:merchant_id/items/:item_id/edit", to: 'items#edit'
-    patch "/merchants/:merchant_id/items/:item_id", to: 'items#update'
-    get "/users", to: "users#index"
-    get "/users/:user_id", to: "users#show"
-    get "/orders/:order_id", to: 'orders#show'
   end
 
   namespace :merchant do
-    get "/dashboard", to: 'dashboard#index'
-    get "/orders/:order_id", to: "orders#show"
-    resources :items, only: [:index, :new, :create]
-    delete "/items/:item_id", to: 'items#destroy'
-    patch "/items/:item_id/toggle", to: 'toggle_items#update'
-    get "/items/:item_id", to: "items#show"
-    get "/items/:item_id/edit", to: 'items#edit'
-    patch "/items/:item_id", to: 'items#update'
+    resources :dashboard, only: [:index]
+    resources :orders, only: [:show]
+    resources :items do
+      patch "/toggle", to: 'toggle_items#update'
+    end
     patch "/items/:item_order_id/fulfill", to: "fulfill#update"
     resources :discounts, except: [:show]
   end
